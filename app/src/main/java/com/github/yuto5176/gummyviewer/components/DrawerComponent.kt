a9026145f3fa1590cda7cf00af5a62ecc216a147
Yuto5176 <b1019003@fun.ac.jp>
2022-05-08 22:39:29 +0900

    ドロワー追加

--- app/src/main/java/com/github/yuto5176/gummyviewer/components/DrawerComponent.kt
+++ app/src/main/java/com/github/yuto5176/gummyviewer/components/DrawerComponent.kt
@@ -1,6 +1,25 @@
 package com.github.yuto5176.gummyviewer.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun DrawerComponent(){}
+fun DrawerComponent(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier
+            .fillMaxSize()
+            .padding(start = 24.dp, top = 48.dp)
+    ) {
+        Text(
+            text = "drawer"
+        )
+    }
+}
--- app/src/main/java/com/github/yuto5176/gummyviewer/navigation/AppNavigation.kt
+++ app/src/main/java/com/github/yuto5176/gummyviewer/navigation/AppNavigation.kt
@@ -1,9 +1,7 @@
 package com.github.yuto5176.gummyviewer.navigation
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.DrawerValue
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberDrawerState
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
@@ -12,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.github.yuto5176.gummyviewer.components.BottomBarComponent
+import com.github.yuto5176.gummyviewer.components.DrawerComponent
 import com.github.yuto5176.gummyviewer.components.TopBarComponent
 import com.github.yuto5176.gummyviewer.ui.screens.favorite.FavoriteScreen
 import com.github.yuto5176.gummyviewer.ui.screens.favorite.FavoriteScreenViewModel
@@ -24,17 +23,40 @@ import kotlinx.coroutines.launch
 @Composable
 fun AppNavigation(startScreen: String) {
     val navController = rememberNavController()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    val openDrawer = {
+        scope.launch {
+            scaffoldState.drawerState.apply {
+                drawerState.open()
+            }
+        }
+    }
+    val closeDrawer = {
+        scope.launch {
+            drawerState.close()
+        }
+    }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
-            TopBarComponent(onButtonClicked = {
-            })
+            TopBarComponent {
+                if (drawerState.isClosed) openDrawer() else closeDrawer()
+            }
         },
         bottomBar = {
             BottomBarComponent(navController = navController)
-        },
-        content = {
+        }
+    ) {
+        ModalDrawer(
+            drawerState = drawerState,
+            gesturesEnabled = drawerState.isOpen,
+            drawerContent = {
+                DrawerComponent()
+            }
+        ) {
             NavHost(navController = navController, startDestination = startScreen) {
                 composable(route = AppScreen.HomeScreen.route) {
                     val homeScreenViewModel = hiltViewModel<HomeScreenViewModel>()
@@ -54,5 +76,5 @@ fun AppNavigation(startScreen: String) {
                 }
             }
         }
-    )
+    }
 }
