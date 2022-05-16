c52688f29d4ba7bfef2389eb99f720fd62f892ee
Yuto5176 <b1019003@fun.ac.jp>
2022-05-16 16:55:55 +0900

    importMd3, fixTheme

--- app/src/main/java/com/github/yuto5176/gummyviewer/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/github/yuto5176/gummyviewer/ui/screens/home/HomeScreen.kt
@@ -3,30 +3,26 @@ package com.github.yuto5176.gummyviewer.ui.screens.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(
     navController: NavController,
     viewModel: HomeScreenViewModel,
-    navigate: () -> Unit
+    navigate:()-> Unit
 ) {
     val gummyCards = viewModel.gummyCards.collectAsState()
-    Surface {
+    Surface{
         Column(
-            modifier = Modifier.padding(8.dp).fillMaxSize(),
+            modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "home")
+        ){
             HomeScreenCard(gummyCards = gummyCards.value, navigate = navigate, modifier = Modifier)
         }
     }
--- app/src/main/java/com/github/yuto5176/gummyviewer/ui/screens/home/HomeScreenCard.kt
+++ app/src/main/java/com/github/yuto5176/gummyviewer/ui/screens/home/HomeScreenCard.kt
@@ -2,46 +2,48 @@ package com.github.yuto5176.gummyviewer.ui.screens.home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.github.yuto5176.gummyviewer.data.model.GummyDetail
-import com.github.yuto5176.gummyviewer.data.model.Image
-import com.github.yuto5176.gummyviewer.navigation.AppScreen
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreenCard(gummyCards: List<GummyDetail>, navigate: () -> Unit, modifier: Modifier) {
-    LazyColumn {
-        items(gummyCards.size) {
+fun HomeScreenCard(gummyCards: List<GummyDetail>, navigate:()-> Unit, modifier: Modifier) {
+    LazyColumn{
+        item{Spacer(modifier = Modifier.padding(top = 10.dp))}
+        items(gummyCards.size){
             Card(
                 modifier = modifier
                     .fillMaxWidth()
-                    .clickable { navigate() },
-                shape = RoundedCornerShape(12.dp),
-                elevation = 4.dp
-            ) {
-                Column(modifier = modifier.fillMaxWidth()) {
+                    .padding(vertical = 10.dp, horizontal = 20.dp)
+                    .clickable{navigate()},
+                shape =RoundedCornerShape(16.dp)
+            ){
+                Column(modifier = modifier.fillMaxWidth()){
                     AsyncImage(
                         model = gummyCards[it].image.url,
                         contentDescription = "image",
                         modifier = modifier
                             .fillMaxWidth()
-                            .clip(shape = RoundedCornerShape(8.dp))
+                            .clip(shape =RoundedCornerShape(8.dp))
                     )
-                    Text(text = gummyCards[it].title)
+                    Text(
+                        modifier = modifier.padding(vertical = 20.dp, horizontal = 15.dp),
+                        text = gummyCards[it].title
+                    )
                 }
             }
         }
     }
 }
-
